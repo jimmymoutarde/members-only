e8bdee1947f767aefdb61ce395fa2a4fe8a01cec
Martin Test <jimmy.moutarde.test@gmail.com>
2022-04-13 15:18:13 +0200

    install devise

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_13_081115) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
